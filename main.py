4fa672e79c39dc10bd2019e16ae9554ad7f86ead
Yoel Leghesse <114878101+yoelleghesse@users.noreply.github.com>
2024-03-21 11:18:06 -0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,18 @@ import cv2
 
 video = cv2.VideoCapture("video.mp4")
 
-width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
-height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 nr_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
 fps = video.get(cv2.CAP_PROP_FPS)
 
-print(width, height, nr_frames, fps)
+timestamp = input('Enter timestamp in hh:mm:ss format: ')
+
+timestamp_list = timestamp.split(':')
+hh, mm, ss = timestamp_list
+timestamp_list_floats = [float(i) for i in timestamp_list]
+hours, minutes, seconds = timestamp_list_floats
+
+frame_nr = hours * 3600 * fps + minutes * 60 * fps 
+
+video.set(1, frame_nr)
+success, frame = video.read()
+cv2.imwrite(f'Frame at {hh}:{mm}:{ss}.jpg', frame)
